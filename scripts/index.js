274270580c1c5ae3c1cdfa63e6abec262212ce96
Nikolay Fomenko <fomen_94@bk.ru>
2022-04-05 21:36:21 +0300

    hotfix: итерация N2

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,31 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const modalWindow = document.querySelector('.popup');
 const modalCloseBtn = modalWindow.querySelector('.popup__close');
-const form = document.querySelector('.popup__form');
-const nameInput = form.querySelector('.popup__field_name');
-const aboutMeInput = form.querySelector('.popup__field_about-me');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+const form = document.querySelector('.popup__form');
+const nameInput = form.querySelector('.popup__field_type_name');
+const aboutMeInput = form.querySelector('.popup__field_type_about-me');
 const popupSave = form.querySelector('.popup__save');
 
-
-function toggleModalWindow () {
-    modalWindow.classList.toggle('popup_opened');
-    nameInput.value = profileTitle.textContent;
-    aboutMeInput.value = profileSubtitle.textContent;
-}
-
-function onOverlayClick (event) {
-    if (event.target === event.currentTarget) {
-        toggleModalWindow();
+function toggleModalWindow() {
+    
+    if (modalWindow.classList.contains('popup_opened') === true) {
+        modalWindow.classList.toggle('popup_opened');
+    } else {
+        modalWindow.classList.toggle('popup_opened');
+        nameInput.value = profileTitle.textContent;
+        aboutMeInput.value = profileSubtitle.textContent;
     }
 }
 
-function saveForm () {
-    modalWindow.classList.toggle('popup_opened');
+function onSubmit(event) {
+    event.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = aboutMeInput.value;
+    toggleModalWindow();
 }
 
 profileEditButton.addEventListener('click', toggleModalWindow);
-
 modalCloseBtn.addEventListener('click', toggleModalWindow);
-
-modalWindow.addEventListener('click', onOverlayClick)
-
-popupSave.addEventListener('click', saveForm)
-
-
-function onSubmit (event) {
-    event.preventDefault();
-}
-
-form.addEventListener('click', onSubmit);
+popupSave.addEventListener('submit', onSubmit);
